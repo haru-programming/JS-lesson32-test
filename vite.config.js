10841c7113f592265114ec99b9ad9e65082a1de0
Haruna Takeda <h136479442@gmail.com>
2023-05-10 05:47:32 +0900

    feat: lesson32 test for PR

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
             extType = 'images';
           }
-          if(extType === 'css') {
+          if (extType === 'css') {
             return `assets/css/content.css`;
           }
           return `assets/${extType}/[name][extname]`;
@@ -24,9 +24,8 @@ export default defineConfig({
       },
       input: {
         main: resolve(__dirname, root, 'index.html'),
-        30: resolve(__dirname, root, '30/index.html'),
-        31: resolve(__dirname, root, '31/index.html')
-      }
-    }
+        lesson32: resolve(__dirname, root, 'lesson32/index.html'),
+      },
+    },
   },
 });
